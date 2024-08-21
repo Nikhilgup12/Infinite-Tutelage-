7111391412e5b59641ddd752bdb3db31e0c8c72b
nikhil <nikhil@gmail.com>
2024-08-21 18:26:59 +0530

    updated code

--- src/components/DashboardPage/index.js
+++ src/components/DashboardPage/index.js
@@ -21,6 +21,15 @@ class DashboardPage extends Component{
                     </div>
                 </div>
                 <div className="dashboard-overall-main-container">
+                <div className="mobile dashboard-search-noti-container">
+                    <div className="search-bar-container">
+                        <button className="search-button">
+                            <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1723974909/search-2-line_m0s0bu.svg" alt="search-icon" className="search-icon" />
+                        </button>
+                        <input type="text" placeholder="Search..." className="search-input" />
+                    </div>
+                    <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1723975519/notification-3-line_pk0iv3.png" /> 
+                    </div>
                     <div className="dashboard-overall-container">
                     <QuickLinks />
                     <OverView />
